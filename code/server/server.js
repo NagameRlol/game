760d3094833c606d60c4f0b6f8009fb569cc4d39
NagyDoesStuff <91905724+NagameRlol@users.noreply.github.com>
2026-05-03 15:46:00 +0000

    paleeeeee work

--- code/server/server.js
+++ code/server/server.js
@@ -1,18 +1,24 @@
 const http = require("http");
 const WebSocket = require("ws");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("OK");
+const server = http.createServer();
+
+const wss = new WebSocket.Server({
+  server,
+  path: "/ws"   // IMPORTANT FIX
 });
 
-const wss = new WebSocket.Server({ server });
-
-server.listen(3000, "0.0.0.0", () => {
-  console.log("Server running");
-});
+let players = {};
 
 wss.on("connection", (ws) => {
   console.log("CONNECTED");
-  ws.send(JSON.stringify({ hello: "world" }));
+
+  ws.on("message", (message) => {
+    const data = JSON.parse(message);
+    ws.send(JSON.stringify({ ok: true }));
+  });
+});
+
+server.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on 3000");
 });
